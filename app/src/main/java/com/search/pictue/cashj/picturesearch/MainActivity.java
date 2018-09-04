9c6bd35aa4a1a5ee40bbd70752fce270f9702c47
cashj <cashjason1@gmail.com>
2018-09-03 23:53:05 -0400

    Adding all of the files

---
+++ app/src/main/java/com/search/pictue/cashj/picturesearch/MainActivity.java
@@ -0,0 +1,13 @@
+package com.search.pictue.cashj.picturesearch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
